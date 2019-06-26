1066a9816fd37ffea335ebf1f8bfcc4e1d03fc50
Pascal Betting <pascal.betting@smartinsurtech.de>
2019-06-26 07:03:05 +0200

    chore: add basic application layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
